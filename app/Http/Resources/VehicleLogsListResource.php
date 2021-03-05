d3a6354227b929543f5d9dfdc64e5d005c76716c
Sly Flores <sly@christian.com.ph>
2021-03-05 12:25:22 +0800

    LOGs BE

---
+++ app/Http/Resources/VehicleLogsListResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VehicleLogsListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'no' => $this->id,
+            'rfid' => $this->rfid,
+            'type' => $this->vehicle->type->name,
+            'brand' => $this->vehicle->brand->name,
+            'model' => $this->vehicle->model,
+            'owner' => "{$this->vehicle->firstname} {$this->vehicle->lastname}",
+            'dateTime' => $this->created_at,
+        ];
+    }
+}
